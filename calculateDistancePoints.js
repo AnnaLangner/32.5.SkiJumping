b1ad3fb157b9b691c3749df43d4123daf093114c
Anna Langner <annalangner06@gmail.com>
2019-09-11 09:15:13 +0200

    Adding functions

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,5 +1,15 @@
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
-...
+    const normal = distance + hillSize + (2 * (distance - kPoint));
+    const big = distance + hillSize + (1.8 * (distance - kPoint));
+    const mammoth = distance + hillSize + (1.2 * (distance - kPoint));
+
+    if (hillSize === 60 && kPoint === 98) {
+        return (normal);
+    } else if (hillSize === 60 && kPoint === 120){
+        return (big);
+    } else if (hillSize === 120 && kPoint === 200) {
+        return (mammoth);
+    }
 };
 
 module.exports = calculateDistancePoints;
--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,5 +1,12 @@
+import {sum} from "lodash/math";
+
 const calculateStylePoints = (styleNotes) => {
-...
+    const minNote = Math.min(...styleNotes);
+    const maxNote = Math.max(...styleNotes);
+    const finallyStyleNotes = sum(...styleNotes) - sum(minNote + maxNote);
+
+    return finallyStyleNotes;
+
 };
 
 module.exports = calculateStylePoints;
